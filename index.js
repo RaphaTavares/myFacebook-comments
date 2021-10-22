43774bff19417b85ea3386c987b55a5c74606114
Raphael Tavares <raphael_tavares2000@hotmail.com>
2021-10-22 18:27:27 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) =>{
+    res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', async (req, res) =>{
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body;
+
+    const postId = req.params.id
+
+    const comments = commentsByPostId[postId] || [];
+
+    comments.push({ id: commentId, content });
+
+    commentsByPostId[postId] = comments;
+
+    await axios.post('http://localhost:4005/events', {
+        type: 'commentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId
+        }
+    })
+
+    res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) =>{
+    console.log('event received: ', req.body.type);
+
+    res.send({});
+})
+
+app.listen(4001, () =>{
+    console.log('Listening on port 4001');
+})
